2f78658f734b91eff0a06043cd6b9cadd3d390fb
Iann Santos <iannsantos8@gmail.com>
2019-11-04 23:36:58 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import './config/reactotron';
+import Header from './components/Header';
+
+import GlobalStyles from './styles/global';
+import Routes from './routes';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyles />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
